848865f9715987d4b51d1b1da92d78fc1cc08fa3
robertnjunge <mubearobert@gmail.com>
2023-09-23 22:59:38 +0300

    completed assignmemt

--- lib/song.py
+++ lib/song.py
@@ -1,80 +1,60 @@
+from . import CURSOR
 
 class Song:
-    count = 0
-    genres = []
-    artists = []
-    genre_count = {}
-    artist_count = {}
+    count = 0  # Initialize the count attribute to zero
+    genres = set()  # Initialize the genres attribute as an empty set
+    artists = set()  # Initialize the artists attribute as an empty set
+    genre_count = {}  # Initialize the genre_count attribute as an empty dictionary
+    artist_count = {}  # Initialize the artist_count attribute as an empty dictionary
 
     def __init__(self, name, artist, genre):
         self.name = name
         self.artist = artist
         self.genre = genre
-        Song.count += 1
-        self.add_to_genres(genre)
-        self.add_to_artists(artist)
-        self.add_to_genre_count(genre)
-        self.add_to_artist_count(artist)
+        Song.count += 1  # Increment the count when a new Song object is created
+        Song.genres.add(genre)  # Add the genre to the set of genres
+        Song.artists.add(artist)  # Add the artist to the set of artists
 
-    def add_to_genres(self, genre):
-        if genre not in Song.genres:
-            Song.genres.append(genre)
-
-    def add_to_artists(self, artist):
-        if artist not in Song.artists:
-            Song.artists.append(artist)
-
-    def add_to_genre_count(self, genre):
+        # Increment the genre count or initialize it if the genre is new
         if genre in Song.genre_count:
             Song.genre_count[genre] += 1
         else:
             Song.genre_count[genre] = 1
 
-#add_to_artist_count(): creates a histogram similar to the one above, but for artists rather than genres.
-    def add_to_artist_count(self, artist):
+        # Increment the artist count or initialize it if the artist is new
         if artist in Song.artist_count:
             Song.artist_count[artist] += 1
         else:
             Song.artist_count[artist] = 1
 
-
-# class Song:
-# #2 Create a class attribute, count add it to the class Song and then add it to the init. set this attribute equal to 0
-#     count = 0
-# #You'll need a class attribute, let's call it genres, that is equal to an empty list.
-#     genres = [""]
-# #You'll need a class attribute, artists, that is equal to an empty list.
-#     artists = [""]
-#     genre_count = {}
-#     artist_count = {}
-# #1 Define your Song class such that an individual song is initialized with a name, artist and genre
-#     def __init__(self, name, artist, genre):
-#         self.name = name
-#         self.artist = artist
-#         self.genre = genre
-# #Whenever a new song is created. Your __init__ method should call a class method add_song_to_count() that increments the value of count by one.
-#         Song.count += 1
-#         Song.genres = "Rap", "Pop", "Rock"
-#         Song.artists = "Jay Z", "Beyonce", "Hall and Oates" 
-#         # for artist_name in artist:
-#         #     self.add_to_artists(artist_name)
+        
+    @classmethod
+    def create(cls, name, album):
+        pass
+        song = Song(name, album)
+        song.save()
+        return song
     
-#     def add_song_to_count():
-#         pass
+    @classmethod
+    def create_table(self):
+        pass
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+        
+        CURSOR.execute(sql)
 
-#     def add_to_genres():
-#         pass
-
-#     def add_to_artists():
-#         pass
-
-#     def add_to_genre_count(genre):
-#         if Song.genre_count.get(genre):
-#             Song.genre_count[genre] += 1
-#         else:
-#             Song.genre_count[genre] = 1
-
-#     def add_to_artist_count():
-#         pass
-
-  
+    def save(self):
+        pass
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+        
+        CURSOR.execute(sql, (self.name, self.album))
+        
+        self.id = CURSOR.execute("SELECT last_insert_rowid() FROM songs").fetchone()[0]
--- lib/testing/song_test.py
+++ lib/testing/song_test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from song import Song
+from lib.song import Song
 
 class TestSong:
     '''Class "Song" in song.py'''
